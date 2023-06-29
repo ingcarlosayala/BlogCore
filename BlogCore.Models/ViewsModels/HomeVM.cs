e66fe75cd67e2754a4f3767467f70b4e425815a1
ingcarlosayala <ing.carlos.ayala12@gmail.com>
2023-06-29 13:31:39 -0400

    Pagina de Inicio

---
+++ BlogCore.Models/ViewsModels/HomeVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogCore.Models.ViewsModels
+{
+    public class HomeVM
+    {
+        public IEnumerable<Articulo> ListaArticulos { get; set; }
+        public IEnumerable<Slider> ListaSlider { get; set; }
+    }
+}
--- BlogCore/Areas/Cliente/Controllers/HomeController.cs
+++ BlogCore/Areas/Cliente/Controllers/HomeController.cs
@@ -1,7 +1,9 @@
-﻿using BlogCore.AccesoDatos.Repositorio.IRepositorio;
+﻿using BlogCore.AccesoDatos.Data;
+using BlogCore.AccesoDatos.Repositorio.IRepositorio;
 using BlogCore.Models;
 using BlogCore.Models.ViewsModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace BlogCore.Areas.Cliente.Controllers
@@ -10,18 +12,32 @@ namespace BlogCore.Areas.Cliente.Controllers
     public class HomeController : Controller
     {
         private readonly IUnidadTrabajo unidadTrabajo;
+        private readonly ApplicationDbContext dbContext;
 
-        public HomeController(IUnidadTrabajo unidadTrabajo)
+        public HomeController(IUnidadTrabajo unidadTrabajo,ApplicationDbContext dbContext)
         {
             this.unidadTrabajo = unidadTrabajo;
+            this.dbContext = dbContext;
         }
 
         [HttpGet]
         public async Task<IActionResult> Index()
         {
-            IEnumerable<Articulo> listaArticulo = await unidadTrabajo.Articulo.ObtenerTodos(incluirPropiedades:"Categoria");
+            HomeVM homeVM = new HomeVM()
+            {
+                ListaArticulos = await unidadTrabajo.Articulo.ObtenerTodos(),
+                ListaSlider = await unidadTrabajo.Slider.ObtenerTodos()
+            };
 
-            return View(listaArticulo);
+            return View(homeVM);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Detalle(int id)
+        {
+            var articulo = await dbContext.Articulo.Include(c => c.Categoria).FirstOrDefaultAsync(a => a.Id == id);
+
+            return View(articulo);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
